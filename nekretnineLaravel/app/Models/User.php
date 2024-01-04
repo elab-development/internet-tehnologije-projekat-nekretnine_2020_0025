5ef63cf1f1726184acd853cad85a07ca6d31dd88
LukaGlavonjic99 <luka.b.glavonjic@gmail.com>
2024-01-04 22:01:43 +0100

    dopunjena klasa User

--- nekretnineLaravel/app/Models/User.php
+++ nekretnineLaravel/app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -41,4 +42,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function properties()
+    {
+        return $this->hasMany(Property::class);
+    }
+
+    public function purchases()
+    {
+        return $this->hasMany(Purchase::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
