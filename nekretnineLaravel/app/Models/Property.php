3336585402c8c076fc6c89674f7269fb10bc947c
LukaGlavonjic99 <luka.b.glavonjic@gmail.com>
2024-01-04 22:00:08 +0100

    kreiran i povezan model

---
+++ nekretnineLaravel/app/Models/Property.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'property_type_id',  
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function propertyType()
+    {
+        return $this->belongsTo(PropertyType::class);
+    }
+
+    public function purchases()
+    {
+        return $this->hasMany(Purchase::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
+}
