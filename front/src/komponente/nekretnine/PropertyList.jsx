f9906c62d2160cf4581b038e8d5684c623431d62
LukaGlavonjic99 <luka.b.glavonjic@gmail.com>
2024-01-11 15:53:44 +0100

    dodata pretraga nekretnina

--- front/src/komponente/nekretnine/PropertyList.jsx
+++ front/src/komponente/nekretnine/PropertyList.jsx
@@ -6,13 +6,15 @@ import PropertyCard from './PropertyCard';
 const PropertyList = () => {
   const [properties, setProperties] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredProp, setFilteredProperties] = useState(properties);
   useEffect(() => {
    
     axios.get('http://127.0.0.1:8000/api/properties')
       .then((response) => {
         console.log(response.data.data)
         setProperties(response.data.data);
+        setFilteredProperties(response.data.data);
         setIsLoading(false);
       })
       .catch((error) => {
@@ -21,16 +23,40 @@ const PropertyList = () => {
       });
   }, []);
 
+
+  const handleSearch = (vrednost) => {
+    setSearchTerm(vrednost)
+    const filteredProperties = properties.filter((property) =>
+      property.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      property.title.toLowerCase().includes(searchTerm.toLowerCase())
+    ); 
+    setFilteredProperties(filteredProperties);
+  };
+
+
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="property-list">
-      {properties.map((property) => (
-         <PropertyCard key={property.id} property={property}></PropertyCard>
-         
-      ))}
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search by description or title"
+          value={searchTerm}
+          onChange={(e) =>handleSearch(e.target.value)}
+        />
+        
+      </div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        filteredProp.map((property) => (
+          <PropertyCard key={property.id} property={property}></PropertyCard>
+        ))
+      )}
     </div>
   );
 };
