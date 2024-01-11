696ce53e5f6e3f01226c04eef29643dd348acb23
LukaGlavonjic99 <luka.b.glavonjic@gmail.com>
2024-01-11 15:57:47 +0100

    dodata paginacija preko reactove biblioteke

--- front/src/komponente/nekretnine/PropertyList.jsx
+++ front/src/komponente/nekretnine/PropertyList.jsx
@@ -2,17 +2,20 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './PropertyList.css';
 import PropertyCard from './PropertyCard';
+import ReactPaginate from 'react-paginate';
 
 const PropertyList = () => {
   const [properties, setProperties] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState('');
-  const [filteredProp, setFilteredProperties] = useState(properties);
+  const [filteredProp, setFilteredProperties] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const propertiesPerPage = 3;  
+
   useEffect(() => {
-   
-    axios.get('http://127.0.0.1:8000/api/properties')
+    axios
+      .get('http://127.0.0.1:8000/api/properties')
       .then((response) => {
-        console.log(response.data.data)
         setProperties(response.data.data);
         setFilteredProperties(response.data.data);
         setIsLoading(false);
@@ -23,21 +26,22 @@ const PropertyList = () => {
       });
   }, []);
 
-
-  const handleSearch = (vrednost) => {
-    setSearchTerm(vrednost)
+  const handleSearch = (value) => {
+    setSearchTerm(value);
     const filteredProperties = properties.filter((property) =>
-      property.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      property.title.toLowerCase().includes(searchTerm.toLowerCase())
-    ); 
+      property.description.toLowerCase().includes(value.toLowerCase()) ||
+      property.title.toLowerCase().includes(value.toLowerCase())
+    );
     setFilteredProperties(filteredProperties);
   };
 
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
 
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const pageCount = Math.ceil(filteredProp.length / propertiesPerPage);
+  const offset = currentPage * propertiesPerPage;
+  const currentProperties = filteredProp.slice(offset, offset + propertiesPerPage);
 
   return (
     <div className="property-list">
@@ -46,16 +50,29 @@ const PropertyList = () => {
           type="text"
           placeholder="Search by description or title"
           value={searchTerm}
-          onChange={(e) =>handleSearch(e.target.value)}
+          onChange={(e) => handleSearch(e.target.value)}
         />
-        
       </div>
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        filteredProp.map((property) => (
-          <PropertyCard key={property.id} property={property}></PropertyCard>
-        ))
+        <>
+            <ReactPaginate
+            previousLabel={'Previous'}
+            nextLabel={'Next'}
+            breakLabel={'...'}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageChange}
+            containerClassName={'pagination'}
+            activeClassName={'active'}
+          />
+          {currentProperties.map((property) => (
+            <PropertyCard key={property.id} property={property}></PropertyCard>
+          ))}
+         
+        </>
       )}
     </div>
   );
