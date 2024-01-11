d98a86db2481a633f3a288744d68aa42eb6edf85
LukaGlavonjic99 <luka.b.glavonjic@gmail.com>
2024-01-11 16:07:52 +0100

    dodati filteri po tipu nekretnine

--- front/src/komponente/nekretnine/PropertyList.jsx
+++ front/src/komponente/nekretnine/PropertyList.jsx
@@ -8,7 +8,8 @@ const PropertyList = () => {
   const [properties, setProperties] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState('');
-  const [filteredProp, setFilteredProperties] = useState([]);
+  const [selectedPropertyType, setSelectedPropertyType] = useState('');
+  const [filteredProperties, setFilteredProperties] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const propertiesPerPage = 3;  
 
@@ -26,7 +27,7 @@ const PropertyList = () => {
       });
   }, []);
 
-  const handleSearch = (value) => {
+  const filterBySearchTerm = (value) => {
     setSearchTerm(value);
     const filteredProperties = properties.filter((property) =>
       property.description.toLowerCase().includes(value.toLowerCase()) ||
@@ -35,13 +36,25 @@ const PropertyList = () => {
     setFilteredProperties(filteredProperties);
   };
 
+  const filterByPropertyType = (type) => {
+    setSelectedPropertyType(type);
+    if (type === '') {
+      setFilteredProperties(properties);
+    } else {
+      const filteredByType = properties.filter(
+        (property) => property.propery_type.name === type
+      );
+      setFilteredProperties(filteredByType);
+    }
+  };
+
   const handlePageChange = ({ selected }) => {
     setCurrentPage(selected);
   };
 
-  const pageCount = Math.ceil(filteredProp.length / propertiesPerPage);
+  const pageCount = Math.ceil(filteredProperties.length / propertiesPerPage);
   const offset = currentPage * propertiesPerPage;
-  const currentProperties = filteredProp.slice(offset, offset + propertiesPerPage);
+  const currentProperties = filteredProperties.slice(offset, offset + propertiesPerPage);
 
   return (
     <div className="property-list">
@@ -50,14 +63,25 @@ const PropertyList = () => {
           type="text"
           placeholder="Search by description or title"
           value={searchTerm}
-          onChange={(e) => handleSearch(e.target.value)}
-        />
+          onChange={(e) => filterBySearchTerm(e.target.value)}
+        />  
+        <select
+          value={selectedPropertyType}
+          onChange={(e) => filterByPropertyType(e.target.value)}
+        >
+          <option value="">All Types</option>
+          <option value="Stan">Stan</option>
+          <option value="Kuća">Kuća</option>
+          <option value="Apartman">Apartman</option>
+          <option value="Vikendica">Vikendica</option>
+          <option value="Poslovni prostor">Poslovni prostor</option>
+        </select>
       </div>
       {isLoading ? (
         <div>Loading...</div>
       ) : (
         <>
-            <ReactPaginate
+          <ReactPaginate
             previousLabel={'Previous'}
             nextLabel={'Next'}
             breakLabel={'...'}
@@ -71,7 +95,6 @@ const PropertyList = () => {
           {currentProperties.map((property) => (
             <PropertyCard key={property.id} property={property}></PropertyCard>
           ))}
-         
         </>
       )}
     </div>
