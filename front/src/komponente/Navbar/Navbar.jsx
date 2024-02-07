f512d481488a8df7c9a02c46e32627c86f410a51
LukaGlavonjic99 <luka.b.glavonjic@gmail.com>
2024-02-07 14:37:49 +0100

    dodata logout opcija

--- front/src/komponente/Navbar/Navbar.jsx
+++ front/src/komponente/Navbar/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';  
 import './Navbar.css';
-const Navbar = ({token,setToken}) => {
- 
+
+const Navbar = ({ token, setToken }) => {
+  const handleLogout = async () => {
+    try {
+      await axios.post('http://127.0.0.1:8000/api/logout', null, {
+        headers: {
+          Authorization: `Bearer ${token}`  
+        }
+      });  
+      setToken(null);  
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
 
   return (
     <nav>
@@ -10,21 +23,31 @@ const Navbar = ({token,setToken}) => {
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/property-list">Property List</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/messages">Messages</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
+        {token ? (
+          <>
+            <li>
+              <Link to="/property-list">Property List</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/messages">Messages</Link>
+            </li>
+            <li>
+              <button onClick={handleLogout}>Logout</button>  
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
