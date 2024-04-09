85efcf6df2866fa048732fa0daf34bec07354eef
LukaGlavonjic99 <luka.b.glavonjic@gmail.com>
2024-04-09 23:34:22 +0200

    omoguceno brisanje nekretnina iz baze od strane prodavaca

--- front/src/komponente/Prodavac/PropertyManagement.jsx
+++ front/src/komponente/Prodavac/PropertyManagement.jsx
@@ -1,9 +1,27 @@
-import React, { useState, useEffect } from 'react'; 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import useNekretnine from '../customHooks/useNekretnine';
-import './PropertyManagement.css' 
+import './PropertyManagement.css';
 
 const PropertyManagement = () => {
-  const { data: properties, isLoading, error } = useNekretnine('http://127.0.0.1:8000/api/properties');
+  const { data: properties, isLoading, error, setData: setProperties } = useNekretnine('http://127.0.0.1:8000/api/properties');
+
+  const token = localStorage.getItem('token'); // Dohvatanje tokena iz sessionStorage-a
+
+  const handleDeleteProperty = async (id) => {
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/properties/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}` // Postavljanje tokena u zaglavlje zahteva
+        }
+      });
+      // Brisanje nekretnine iz lokalne memorije
+      const updatedProperties = properties.filter(property => property.id !== id);
+      setProperties(updatedProperties);
+    } catch (error) {
+      console.error('Error deleting property:', error);
+    }
+  };
 
   return (
     <div className="property-container">
@@ -20,6 +38,7 @@ const PropertyManagement = () => {
               <th>Description</th>
               <th>Price</th>
               <th>Bedrooms</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
@@ -29,6 +48,9 @@ const PropertyManagement = () => {
                 <td>{property.description}</td>
                 <td>{property.price}</td>
                 <td>{property.bedrooms}</td>
+                <td>
+                  <button onClick={() => handleDeleteProperty(property.id)}>Delete</button>
+                </td>
               </tr>
             ))}
           </tbody>
